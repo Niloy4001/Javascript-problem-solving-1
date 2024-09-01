83e144694de4ec5b500cc2ce33a96a5fccfe9ec2
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-01 07:42:20 +0600

    Even or Odd finding problem solved

---
+++ problem.js
@@ -0,0 +1,27 @@
+
+
+/////////////////////////////////////////////////////////////
+// 1. Even or Odd
+// Write a JavaScript program to check if a given number is even or odd using the modulus operator.
+
+let num = -7;
+
+if (num === 0) {
+    console.log("The number is not Even or Odd, it is 0" );
+}else{
+    if(num % 2 === 0) {
+        console.log("The number ("+ num +") is Even" );
+    }
+    else{
+        console.log("The number "+ num +" is Odd" );
+    }
+}
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////
