2b92147af9e4285c594383c57387ea68401eba8f
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-05 08:00:58 +0600

    Password strong or weak

--- problem.js
+++ problem.js
@@ -4,18 +4,18 @@
 // 1. Even or Odd
 // Write a JavaScript program to check if a given number is even or odd using the modulus operator.
 
-// let num = -7;
+let num = -7;
 
-// if (num === 0) {
-//     console.log("The number is not Even or Odd, it is 0" );
-// }else{
-//     if(num % 2 === 0) {
-//         console.log("The number ("+ num +") is Even" );
-//     }
-//     else{
-//         console.log("The number "+ num +" is Odd" );
-//     }
-// }
+if (num === 0) {
+    console.log("The number is not Even or Odd, it is 0");
+} else {
+    if (num % 2 === 0) {
+        console.log("The number (" + num + ") is Even");
+    }
+    else {
+        console.log("The number " + num + " is Odd");
+    }
+}
 
 
 
@@ -28,22 +28,22 @@
 // 2. Largest of Three Numbers
 // Create a program that takes three numbers as input and uses if-else statements to find and print the largest number.
 
-// let num1 = -79;
-// let num2 = -65;
-// let num3 = -28;
+let num1 = -79;
+let num2 = -65;
+let num3 = -28;
 
-// if(num1 > num2 && num1 > num3){
-//     console.log("The number ("+num1 +") is largest out of three")
-// }
-// else if(num2 > num1 && num2 > num3){
-//     console.log("The number ("+num2 +") is largest out of three")
-// }
-// else if(num3 > num1 && num3 > num2){
-//     console.log("The number ("+num3 +") is largest out of three")
-// }
-// else{
-//     console.log("Please don't input at least two same number")
-// }
+if (num1 > num2 && num1 > num3) {
+    console.log("The number (" + num1 + ") is largest out of three")
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log("The number (" + num2 + ") is largest out of three")
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log("The number (" + num3 + ") is largest out of three")
+}
+else {
+    console.log("Please don't input at least two same number")
+}
 
 
 
@@ -57,15 +57,15 @@
 // 3. Check Leap Year
 // Write a function to determine whether a given year is a leap year or not. Remember that a leap year is divisible by 4, but if it is a century year (like 1900 or 2000), it must also be divisible by 400.
 
-// let year = 800;
+let year = 800;
 
-// if( year % 4 === 0 && year % 100 !== 0 ){
-//     console.log("it is a leap year")
-// }else if( year % 4 === 0 && year > 999 && year % 400 === 0 ){ 
-//     console.log("it is a leap year")
-// }else{
-//     console.log("its not leap year")
-// }
+if (year % 4 === 0 && year % 100 !== 0) {
+    console.log("it is a leap year")
+} else if (year % 4 === 0 && year > 999 && year % 400 === 0) {
+    console.log("it is a leap year")
+} else {
+    console.log("its not leap year")
+}
 
 
 
@@ -84,23 +84,23 @@
 // 60 to 69: D
 // Below 60: F
 
-// let mark = 95;
+let mark = 95;
 
-// if(mark >= 90 && mark <= 100 ){
-//     console.log("Your grade is A")
-// }
-// else if(mark >= 80 && mark <= 89 ){
-//     console.log("Your grade is B")
-// }
-// else if(mark >= 70 && mark <= 79 ){
-//     console.log("Your grade is C")
-// }
-// else if(mark >= 60 && mark <= 69 ){
-//     console.log("Your grade is D")
-// }
-// else if(mark < 60  ){
-//     console.log("Your grade is F")
-// }
+if (mark >= 90 && mark <= 100) {
+    console.log("Your grade is A")
+}
+else if (mark >= 80 && mark <= 89) {
+    console.log("Your grade is B")
+}
+else if (mark >= 70 && mark <= 79) {
+    console.log("Your grade is C")
+}
+else if (mark >= 60 && mark <= 69) {
+    console.log("Your grade is D")
+}
+else if (mark < 60) {
+    console.log("Your grade is F")
+}
 
 
 
@@ -114,30 +114,30 @@
 // 5. Simple Calculator
 // Write a simple calculator program that takes two numbers and an operator (+, -, *, /) as input. Use if-else to perform the corresponding operation and display the result.
 
-// let number1 = 7;
-// let number2 = 4;
-// let operator = "-";
+let number1 = 7;
+let number2 = 4;
+let operator = "-";
 
-// let operation;
+let operation;
 
-// if(operator === "+"){
-//     operation = number1 + number2;
-//     console.log(operation);
-// }
-// else if(operator === "-"){
-//     operation = number1 - number2;
-//     console.log(operation);
-// }
-// else if(operator === "*"){
-//     operation = number1 * number2;
-//     console.log(operation);
-// }
-// else if(operator === "/"){
-//     operation = number1 / number2;
-//     console.log(operation);
-// }else{
-//     console.log("the operation is invalid");
-// }
+if (operator === "+") {
+    operation = number1 + number2;
+    console.log(operation);
+}
+else if (operator === "-") {
+    operation = number1 - number2;
+    console.log(operation);
+}
+else if (operator === "*") {
+    operation = number1 * number2;
+    console.log(operation);
+}
+else if (operator === "/") {
+    operation = number1 / number2;
+    console.log(operation);
+} else {
+    console.log("the operation is invalid");
+}
 
 
 
@@ -151,18 +151,18 @@
 // Create a function that takes a number as input and checks whether it is positive, negative, or zero using if-else statements.
 
 
-// let aNumber = 0;
+let aNumber = 0;
 
-// if (aNumber > 0) {
-//     console.log("Positive");
-    
-// }else if (aNumber < 0) {
-//     console.log('Negative');
-    
-// }else{
-//     console.log('It is zero ');
-    
-// }
+if (aNumber > 0) {
+    console.log("Positive");
+
+} else if (aNumber < 0) {
+    console.log('Negative');
+
+} else {
+    console.log('It is zero ');
+
+}
 
 
 
@@ -185,15 +185,15 @@
 // 7. Vowel or Consonant
 // Write a program that takes a single character as input and checks whether it is a vowel (a, e, i, o, u) or a consonant.
 
-// let character = 'c';
+let character = 'c';
 
-// if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u' ) {
-//     console.log('vowel');
-    
-// }else{
-//     console.log('consonant');
-    
-// }
+if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+    console.log('vowel');
+
+} else {
+    console.log('consonant');
+
+}
 
 
 
@@ -227,9 +227,9 @@ let age = 21;
 if (age >= 18) {
     console.log('You can go for vote');
 
-}else{
+} else {
     console.log('You cannot vote');
-    
+
 }
 
 
@@ -254,10 +254,62 @@ if (age >= 18) {
 // 9. Check Password Strength
 // Write a function to check the strength of a password. If the password length is greater than or equal to 8 characters, print "Strong", otherwise print "Weak".
 
+let password = 'auereweru';
+
+if (password.length >= 8) {
+    console.log('strong');
+
+} else {
+    console.log('weak');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 10. FizzBuzz
 // Write a program that prints the numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number and for multiples of 5, print "Buzz". For numbers that are multiples of both 3 and 5, print "FizzBuzz".
 
 // These questions will give you a good mix of basic to intermediate practice with operators and if-else conditions in JavaScript.
+
+for (let index = 1; index <= 100; index++) {
+
+    if (index % 3 === 0 && index % 5 === 0) {
+        console.log('FizzBuzz');
+        continue;
+    }
+    else if (index % 3 === 0) {
+        console.log('Fizz');
+        continue;
+
+    }
+    else if (index % 5 === 0) {
+        console.log('Buzz');
+        continue;
+    }
+
+
+    console.log(index);
+
+
+}
 
 
 
